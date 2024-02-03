3d7ff8fae3dae0ea7f9b0bc2a57acd1e08434438
Arseniy Borodulin <senya.borodulin@mail.ru>
2024-02-03 03:23:43 +0300

    --amend

--- _deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arseniiborodulin/Desktop/coding/BigFloatCPP/_deps/googletest-src/googlemock
-# Build directory: /Users/arseniiborodulin/Desktop/coding/BigFloatCPP/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
