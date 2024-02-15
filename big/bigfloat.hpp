2298428202ce3b6da833cba01e8e1cea2c45c053
Arseniy Borodulin <senya.borodulin@mail.ru>
2024-02-15 20:29:15 +0300

    fixed plus and minus to good functions without glinomesivo

--- big/bigfloat.hpp
+++ big/bigfloat.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <vector>
+#include <sstream>
+#include <iostream>
 
 
 
@@ -12,21 +14,7 @@ public:
     std::vector<int> number;
     bool minus;
 
-    static int sum_arrays_left(
-        std::vector<int>::iterator, std::vector<int>::iterator,
-        std::vector<int>::iterator, std::vector<int>::iterator,
-        std::vector<int>&, int = 0, bool=true, bool = true
-    );
-    static int sum_arrays_right(
-        std::vector<int>::iterator, std::vector<int>::iterator,
-        std::vector<int>::iterator, std::vector<int>::iterator,
-        std::vector<int>&, int = 0, bool = true
-    );
-    static int sub_arrays_left(
-        std::vector<int>::iterator, std::vector<int>::iterator,
-        std::vector<int>::iterator, std::vector<int>::iterator,
-        std::vector<int>&, int = 0, bool = true
-    );
+    int get_num(int index, BigFloat& to_num);
 //    static int sub_arrays_right(
 //        std::vector<int>::iterator, std::vector<int>::iterator,
 //        std::vector<int>::iterator, std::vector<int>::iterator,
@@ -46,6 +34,14 @@ public:
     BigFloat operator-(BigFloat&);
 //    BigFloat operator*(BigFloat &b);
 //    BigFloat operator/(BigFloat &b);
+
+    bool operator<(BigFloat&);
+    bool operator<=(BigFloat&);
+    bool operator>=(BigFloat&);
+    bool operator>(BigFloat&);
+    bool operator==(BigFloat&);
+
+    friend std::ostream& operator<< (std::ostream&, BigFloat&);
 
     std::string raw_number();
     std::string str();
