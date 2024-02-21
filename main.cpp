9d4728b3a84bdea9f1ad404130e0beeec391e013
Arseniy Borodulin <senya.borodulin@mail.ru>
2024-02-22 00:13:19 +0300

    fix: cmake error

--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 #include <big/bigfloat.hpp>
+#include "gtest/gtest.h"
 
 
 //void *$(){[&](...){$:[](){({$:;_:[](){},""[+-(&&$<&&_)];});}();}([=](){});}
 
 int main() {
-    BigFloat a(4);
-    BigFloat b(51);
-    BigFloat pi = BigFloat::get_pi(200);
-    std::cout << pi << std::endl;
-    BigFloat a1("3.14");
-    BigFloat b1("0.0000001");
-    BigFloat ans = a1 + b1;
-    std::cout << ans.raw_number();
+//    BigFloat pi = BigFloat::get_pi(200);
+//    std::cout << pi << std::endl;
     return 0;
 }
