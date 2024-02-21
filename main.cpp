0dd17939fb5df5fb3aae2de065b5898c121ce1a9
Arseniy Borodulin <senya.borodulin@mail.ru>
2024-02-22 00:14:36 +0300

    fix: imports

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <big/bigfloat.hpp>
-#include "gtest/gtest.h"
 
 
 //void *$(){[&](...){$:[](){({$:;_:[](){},""[+-(&&$<&&_)];});}();}([=](){});}
 
 int main() {
-//    BigFloat pi = BigFloat::get_pi(200);
-//    std::cout << pi << std::endl;
+    BigFloat pi = BigFloat::get_pi(200);
+    std::cout << pi << std::endl;
     return 0;
 }
