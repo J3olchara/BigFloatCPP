2ae6803c6f1221df86b7d554a87979d59d421ce1
Arseniy Borodulin <senya.borodulin@mail.ru>
2024-02-22 01:04:28 +0300

    feat: readme and test dependencies

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <big/bigfloat.hpp>
+#include <chrono>
+
+
 
 
 int main() {
-    BigFloat pi = BigFloat::get_pi(10);
-    std::cout << pi << std::endl;
+    clock_t start = clock();
+    BigFloat pi = BigFloat::get_pi(100);
+    clock_t end = clock();
+    float time_to_compute = static_cast<float>(end - start) / CLOCKS_PER_SEC;
+    std::cout << "PI: " << pi << std::endl;
+    std::cout << "To compute pi required " << time_to_compute << " seconds";
     return 0;
 }
